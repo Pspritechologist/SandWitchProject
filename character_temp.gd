6b80fd53e64a0ee224cc08ef5168479a81367479
DEATHB4DEFEAT <zachcaffee@outlook.com>
2025-01-25 22:01:16 -0800

    un constant the speed and jump vel

--- character_temp.gd
+++ character_temp.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var speed: float = 5.0
+@export var jump_velocity: float = 4.5
 
 @export_category("Inputs")
 @export var mouse_sensitivity: float = 100
@@ -19,17 +19,17 @@ func _physics_process(delta: float) -> void:
 	
 	# Handle jump.
 	if Input.is_action_just_pressed(jump) and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 		
 	
 	var input_dir := Input.get_vector(move_left, move_right, move_foward, move_backward)
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 	
 	move_and_slide()
 	
