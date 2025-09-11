e3851aa233864ad1ed88f69149cfef0f02c0b4bb
Pspritechologist <naaronn@gmail.com>
2025-09-10 21:00:26 -0400

    Cursor stops moving when rotating.

--- rts/rts_camera.gd
+++ rts/rts_camera.gd
@@ -1,6 +1,8 @@
 class_name RtsCamera extends Camera3D
 
-@export var cursor_follow_mouse := true
+@export var do_follow_mouse := true
+
+var _do_follow_mouse := true
 
 @onready var _raycast: RayCast3D = $RayCast3D
 @onready var _cursor: MeshInstance3D = $RtsCursor
@@ -41,16 +43,15 @@ func set_cursor_color(color: Color) -> void:
 	
 
 func _process(delta: float) -> void:
-	if !cursor_follow_mouse: return
-	
-	var mouse_pos := get_viewport().get_mouse_position()
-	var normal := self.project_ray_normal(mouse_pos)
-	
-	var facing := _raycast.global_position + normal
-	_raycast.look_at(facing)
-	
-	var hit_pos := _raycast.get_collision_point()
-	_cursor.global_position = hit_pos
+	if _do_follow_mouse && do_follow_mouse:
+		var mouse_pos := get_viewport().get_mouse_position()
+		var normal := self.project_ray_normal(mouse_pos)
+		
+		var facing := _raycast.global_position + normal
+		_raycast.look_at(facing)
+		
+		var hit_pos := _raycast.get_collision_point()
+		_cursor.global_position = hit_pos
 	
 	_cursor.rotate_y(deg_to_rad(45) * delta)
 	
@@ -60,16 +61,19 @@ func _unhandled_input(event: InputEvent) -> void:
 		var mouse_event := event as InputEventMouseMotion
 		if Input.is_action_pressed(&"rotate", true):
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			_do_follow_mouse = false
 			rotate_y(deg_to_rad(-mouse_event.relative.x * 0.1))
 			var new_rot := rotation_degrees
 			new_rot.x = clamp(new_rot.x - mouse_event.relative.y * 0.1, -89.9, 89.9)
 			rotation_degrees = new_rot
 		elif Input.is_action_pressed(&"pan", true):
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			_do_follow_mouse = false
 			var right := -transform.basis.x.normalized()
 			var forward := -transform.basis.z.normalized()
 			translate_object_local(right * mouse_event.relative.x * 0.1)
 			translate_object_local(forward * mouse_event.relative.y * 0.1)
 	elif event.is_action_released(&"pan") or event.is_action_released(&"rotate"):
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			_do_follow_mouse = true
 	
